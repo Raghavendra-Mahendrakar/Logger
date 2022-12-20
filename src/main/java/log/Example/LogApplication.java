f7e40d5c41a7f316afa902191a5ae67267c8a087
Raghavendra-Mahendrakar <raghukm338@gmail.com>
2022-12-20 11:21:35 +0530

    push

--- src/main/java/log/Example/LogApplication.java
+++ src/main/java/log/Example/LogApplication.java
@@ -1,8 +1,14 @@
 package log.Example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 
 public class LogApplication {
@@ -11,4 +17,21 @@ public class LogApplication {
 		SpringApplication.run(LogApplication.class, args);
 	}
 
+	Logger logger = LoggerFactory.getLogger(LogApplication.class);
+
+	@RequestMapping("/")
+	public String home() {
+		logger.error("error log");
+		logger.trace("home method accessed");
+		return "hello from SB";
+	}
+
+	@RequestMapping("/test/{name}")
+	public String test(@PathVariable String name) {
+		logger.debug("Request {}", name);
+		String response = "hi " + name + " welcome";
+		logger.debug("Response {}", name);
+		return response;
+
+	}
 }
